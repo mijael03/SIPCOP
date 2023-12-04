10480c3184c11809f783b1f5efaf27bff3dea3cc
aplicativosinnova <aplicativosinnova@gmail.com>
2023-12-04 10:11:51 -0500

    Add changes for handle map interaction and delete packagelock

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ import puppeteer from "puppeteer";
   await page.waitForFunction(() => {
     const loaderSpinner = document.getElementById('loaderSpinner');
     return loaderSpinner.style.display === 'none';
-  }, {timeout: 45000});
+  }, { timeout: 45000 });
   console.log("First screen charge completed")
   await page.click('button[class="mr-2 mb-2 mat-raised-button mat-button-base mat-accent"]')
   await page.waitForSelector('div[formarrayname="serenos"]')
@@ -57,7 +57,7 @@ import puppeteer from "puppeteer";
   await page.waitForFunction(() => {
     const loaderSpinner = document.getElementById('loaderSpinner');
     return loaderSpinner.style.display === 'none';
-  }, {timeout: 3000});
+  }, { timeout: 30000 });
   const selectorOrigen = 'mat-select[formcontrolname="idOrigen"]'
   await page.waitForSelector(selectorOrigen)
   await page.click('mat-select[formcontrolname="idOrigen"]')
@@ -83,7 +83,7 @@ import puppeteer from "puppeteer";
   const selectTurno = await page.$x('//mat-option[contains(.//span, " MAÑANA ")]');
   await selectTurno[0].click()
   await page.click('button[aria-label="Open calendar"]')
-  const selectFecha = await page.$x('//table/tbody/tr/td[contains(@aria-label, "23 de noviembre de 2023")]');
+  const selectFecha = await page.$x('//table/tbody/tr/td[contains(@aria-label, "1 de diciembre de 2023")]');
   await selectFecha[0].click()
   const selectHour = await page.$x('//ngx-mat-timepicker/form/table/tbody/tr/td/mat-form-field/div/div/div/input[contains(@formcontrolname, "hour")]');
   await selectHour[0].evaluate(input => input.value = '');
@@ -96,7 +96,7 @@ import puppeteer from "puppeteer";
   await selectSecond[0].type("08")
   const doneButton = await page.$x('//div/button/span[contains(.//mat-icon,"done")]');
   await doneButton[0].click()
-  const [siguienteButton, siguienteButton2,siguienteButton3] = await page.$x('//button[contains(.//span,"Siguiente")]');
+  const [siguienteButton, siguienteButton2, siguienteButton3] = await page.$x('//button[contains(.//span,"Siguiente")]');
   await siguienteButton.click()
   await waitOneSecond()
   const selectorGenerico = 'mat-select[formcontrolname="idGenerico"]'
@@ -113,14 +113,17 @@ import puppeteer from "puppeteer";
   await selectSubTipo.click()
   await waitOneSecond()
   await page.click('mat-select[formcontrolname="idModalidad"]')
-  await waitOneSecond()
+  await page.waitForFunction(() => {
+    const loaderSpinner = document.getElementById('loaderSpinner');
+    return loaderSpinner.style.display === 'none';
+  });
   const selectModalidad = await page.$x('//mat-option[contains(.//span, "CONSUMO DE LICOR EN LA VIA PUBLICA O INTERIOR DE VEHICULO")]');
   await selectModalidad[0].click()
   await waitOneSecond()
   await siguienteButton2.click()
   const selectorIdentificacionAutor = 'mat-select[formcontrolname="idIdentificacionAutor"]';
   await page.waitForSelector(selectorIdentificacionAutor, { visible: true, timeout: 3000 });
-    // Once the element is present, click it
+  // Once the element is present, click it
   await page.click(selectorIdentificacionAutor)
   const [selectIdentificacionAutor] = await page.$x('//mat-option[contains(.//span, "NO IDENTIFICADO")]');
   await selectIdentificacionAutor.click()
@@ -132,11 +135,15 @@ import puppeteer from "puppeteer";
   await siguienteButton3.click()
   const selectorTipoVia = 'mat-select[formcontrolname="idTipoVia"]';
   await page.waitForSelector(selectorTipoVia, { visible: true, timeout: 3000 });
-    // Once the element is present, click it
+  // Once the element is present, click it
   await page.click(selectorTipoVia)
+  await page.waitForFunction(() => {
+    const loaderSpinner = document.getElementById('loaderSpinner');
+    return loaderSpinner.style.display === 'none';
+  });
   const [selectTipoVia] = await page.$x('//mat-option[contains(.//span, "CALLE")]');
   selectTipoVia.click()
-  const selectorDireccion= 'input[formcontrolname="direccion"]';
+  const selectorDireccion = 'input[formcontrolname="direccion"]';
   await page.type(selectorDireccion, 'Ballon')
   const selectorTipoZona = 'mat-select[formcontrolname="idTipoZona"]'
   await page.waitForSelector(selectorTipoZona)
@@ -145,26 +152,96 @@ import puppeteer from "puppeteer";
   selectTipoZona.click()
   //await waitToMapCharge()
   await waitOneSecond()
+  const selector = '.esri-view-root';
+  const element = await page.$(selector)
+  await waitOneSecond()
+  await waitOneSecond()
+  await page.evaluate((element) => {
+    element.scrollIntoView({ behavior: 'auto', block: 'center', inline: 'center' })
+  }, element)
+  await waitOneSecond()
+
+  const screenPosition = [
+    { x: 1062, y: 174 },
+    { x: 1018, y: 371 },
+    { x: 1114, y: 427 },
+    { x: 1102, y: 352 },
+    { x: 1064, y: 465 },
+    { x: 1144, y: 404 },
+    { x: 1142, y: 345 },
+    { x: 1079, y: 357 },
+    { x: 1091, y: 318 },
+    { x: 1066, y: 293 }
+  ]
+
+  /* for (const positions in screenPosition) {
+    
+  } */
+  // for(screenPosition)
+  for (let y = 0; y <= 9; y += 1) {
+    await page.mouse.move(screenPosition[y].x, screenPosition[y].y);
+    await waitForMovement()
+    let divContent = await page.evaluate((selector) => {
+      const div = document.querySelector(selector)
+      return div ? div.textContent : null
+    }, '#coordsWidget')
+    if (divContent) {
+      console.log("SCREEN POSITION: ", `x: ${screenPosition[y].x} y: ${screenPosition[y].y}`)
+      console.log("COORDENADAS: ", `x: ${divContent.slice(8, 16)} y: ${divContent.slice(17, 25)}`)
+      console.log("------------------------------------")
+    } else {
+      console.log("div not found")
+    }
+  }
+  /* await Promise.all(
+    screenPosition.map(async (positions) => {
+      await page.mouse.move(positions.x, positions.y);
+      await waitForMovement()
+      let divContent = await page.evaluate((selector) => {
+        const div = document.querySelector(selector)
+        return div ? div.textContent : null
+      }, '#coordsWidget')
+      console.log("DIV CONTENT", divContent)
+      if (divContent) {
+        console.log("SCREEN POSITION: ", `x: ${positions.x} y: ${positions.y}`)
+        console.log("COORDENADAS: ", `x: ${divContent.slice(8, 16)} y: ${divContent.slice(17, 25)}`)
+      } else {
+        console.log("div not found")
+      }
+    })
+  ) */
+  /* await page.mouse.move(1102, 352);
+  let divContent = await page.evaluate((selector) => {
+    const div = document.querySelector(selector)
+    console.log("HTML", div.innerHTML)
+    console.log("TEXT CONTENT", div.textContent)
+    return div ? div.textContent : null
+  }, '#coordsWidget')
+  console.log("DIV CONTENT", divContent)
+  if (divContent) {
+    console.log("SCREEN POSITION: ", `x: ${1102} y: ${352}`)
+    console.log("COORDENADAS: ", `x: ${divContent.slice(8, 16)} y: ${divContent.slice(17, 25)}`)
+  } else {
+    console.log("div not found")
+  }
+  await waitForMovement() */
 
   await page.screenshot({ path: 'example.png' });
-  const selector = '.esri-view-root';
-  await page.waitForSelector(selector);
-  // Extract the content inside the specified tag
-  const content = await page.$eval(selector, (element) => {
-    return element.innerHTML;
-  });
-
-  console.log('Content inside map component:', content);
   await browser.close();
 })();
 
 async function waitOneSecond() {
   return new Promise(resolve => {
-    setTimeout(resolve, 1000); 
+    setTimeout(resolve, 1000);
+  });
+}
+async function waitForMovement() {
+  return new Promise(resolve => {
+    setTimeout(resolve, 100);
   });
 }
 async function waitToMapCharge() {
   return new Promise(resolve => {
-    setTimeout(resolve, 60000); 
+    setTimeout(resolve, 60000);
   });
 }
